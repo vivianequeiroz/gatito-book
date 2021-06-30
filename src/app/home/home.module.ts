fce70c3923aadf7f197f8085ebc5644c4ca91295
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-30 18:25:39 -0300

    chore: login component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
