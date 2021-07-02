a711290c5f8508d3326873dfed00981e7e92be85
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-02 18:31:43 -0300

    chore: subscribe component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home.component";
 import { LoginComponent } from "./login/login.component";
+import { NovoUsuarioComponent } from "./novo-usuario/novo-usuario.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
     children: [{ path: "", component: LoginComponent }],
+  },
+  {
+    path: "novo-usuario",
+    component: NovoUsuarioComponent,
   },
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from "./home.component";
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { MensagemComponent } from '../componentes/mensagem/mensagem.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule, FormsModule],
-  declarations: [HomeComponent, LoginComponent, MensagemComponent],
+  declarations: [HomeComponent, LoginComponent, MensagemComponent, NovoUsuarioComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
