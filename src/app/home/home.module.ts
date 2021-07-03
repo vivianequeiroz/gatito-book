1f3558650354d50bdf5adfed9f993f31bdfc4cd5
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-03 20:49:22 -0300

    chore: binding reactive form with its data model

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from "@angular/common";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
-import { MensagemComponent } from '../componentes/mensagem/mensagem.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, FormsModule],
-  declarations: [HomeComponent, LoginComponent, MensagemComponent, NovoUsuarioComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule, ReactiveFormsModule],
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { NovoUsuarioService } from "./novo-usuario.service";
 
 @Component({
-  selector: 'app-novo-usuario',
-  templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  selector: "app-novo-usuario",
+  templateUrl: "./novo-usuario.component.html",
+  styleUrls: ["./novo-usuario.component.css"],
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [""],
+      fullName: [""],
+      userName: [""],
+      password:[""],
+    });
   }
-
 }
