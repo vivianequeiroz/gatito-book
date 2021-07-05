fd177213c6c0cf9ca6ea343f2d39cbb64560c143
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-05 15:54:19 -0300

    chore: token service

---
+++ src/app/autenticacao/token.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+
+const KEY = "token";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TokenService {
+  returnToken() {
+    return (localStorage.getItem(KEY)) ?? "";
+  }
+
+  salvaToken(token: string) {
+    localStorage.setItem(KEY, token);
+  }
+
+  excluiToken() {
+    localStorage.removeItem(KEY);
+  }
+
+  possuiToken() {
+    return !! (this.returnToken);
+  }
+}
--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import { minusculoValidator } from "./minusculo.validator";
 import { NovoUsuario } from "./novo-usuario";
 import { NovoUsuarioService } from "./novo-usuario.service";
 import { UsuarioExisteService } from "./usuario-existe.service";
-import { usuarioSenhaIguaisValidator } from "./usuario-senha-iguais.validator";
+// import { usuarioSenhaIguaisValidator } from "./usuario-senha-iguais.validator";
 @Component({
   selector: "app-novo-usuario",
   templateUrl: "./novo-usuario.component.html",
@@ -16,7 +17,8 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private novoUsuarioService: NovoUsuarioService,
-    private usuarioExistenteService: UsuarioExisteService
+    private usuarioExistenteService: UsuarioExisteService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -30,14 +32,24 @@ export class NovoUsuarioComponent implements OnInit {
           [this.usuarioExistenteService.usuarioJaExiste()],
         ],
         password: [""],
-      },
-      {
-        validators: [usuarioSenhaIguaisValidator],
-      }
+      } //,
+      // {
+      //   validators: [usuarioSenhaIguaisValidator],
+      // }
     );
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(
+        () => {
+          this.router.navigate([""]);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
