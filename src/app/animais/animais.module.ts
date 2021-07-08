0fb09dd32f50c932e343a10902226dffcefe66d6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-07 21:15:41 -0300

    chore: card component for animals list

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { AnimalComponent } from './animal/animal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AnimaisRoutingModule
   ],
-  declarations: [ListaAnimaisComponent]
+  declarations: [ListaAnimaisComponent, AnimalComponent]
 })
 export class AnimaisModule { }
---
+++ src/app/componentes/cartao/cartao.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cartao',
+  templateUrl: './cartao.component.html',
+  styleUrls: ['./cartao.component.css']
+})
+export class CartaoComponent implements OnInit {
+
+@Input() titulo = '';
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
