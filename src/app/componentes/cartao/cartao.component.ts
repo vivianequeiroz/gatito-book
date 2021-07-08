70d4d017aceecc81ce54bc0504eb34d6cde1c0f5
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-07 21:16:04 -0300

    chore: card component for animals list

--- src/app/componentes/cartao/cartao.component.ts
+++ src/app/componentes/cartao/cartao.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cartao',
-  templateUrl: './cartao.component.html',
-  styleUrls: ['./cartao.component.css']
+  selector: "app-cartao",
+  templateUrl: "./cartao.component.html",
+  styleUrls: ["./cartao.component.css"],
 })
 export class CartaoComponent implements OnInit {
+  @Input() titulo = "";
 
-@Input() titulo = '';
+  constructor() {}
 
-
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
