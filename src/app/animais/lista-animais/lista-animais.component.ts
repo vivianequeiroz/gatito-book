b89ed927023c70dd5cc7ff845dd62bffcac5dd7f
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-13 10:55:19 -0300

    fix: display of animals photos

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsuarioService } from "src/app/autenticacao/usuario/usuario.service";
+import { Animais } from "../animais";
+import { AnimaisService } from "../animais.service";
 
 @Component({
-  selector: 'app-lista-animais',
-  templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  selector: "app-lista-animais",
+  templateUrl: "./lista-animais.component.html",
+  styleUrls: ["./lista-animais.component.css"],
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais!: Animais;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      if(userName) {
+        this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+          this.animais = animais;
+        })
+      }
+    })
   }
-
 }
