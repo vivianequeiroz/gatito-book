369a2ec60c765a56ea41fed823896ab35a2c43d6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-13 11:28:49 -0300

    chore: avoiding home redirecting with user is already logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AutenticacaoGuard } from "./autenticacao/autenticacao.guard";
+import { LoginGuard } from "./autenticacao/login.guard";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home',
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home",
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    path: "home",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
-    path: 'animais',
+    path: "animais",
     loadChildren: () =>
-      import('./animais/animais.module').then((m) => m.AnimaisModule),
+      import("./animais/animais.module").then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard],
   },
 ];
 
