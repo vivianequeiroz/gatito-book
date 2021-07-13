832b4e149c6fc10e37492d46c8993c7e548667c5
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-13 11:48:22 -0300

    chore: activated route to access animal photo detail

--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+import { Animal } from "../animais";
+import { AnimaisService } from "../animais.service";
 
 @Component({
-  selector: 'app-detalhe-animal',
-  templateUrl: './detalhe-animal.component.html',
-  styleUrls: ['./detalhe-animal.component.css']
+  selector: "app-detalhe-animal",
+  templateUrl: "./detalhe-animal.component.html",
+  styleUrls: ["./detalhe-animal.component.css"],
 })
 export class DetalheAnimalComponent implements OnInit {
-
-  constructor() { }
+  animalId!: number;
+  animal$!: Observable<Animal>;
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorID(this.animalId);
   }
-
 }
