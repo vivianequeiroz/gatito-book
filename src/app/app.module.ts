19dbb44f41a5e664ec43864b64fc6c28c132a9d4
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-15 16:49:06 -0300

    chore: registering interceptor in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AutenticacaoModule } from "./autenticacao/autenticacao.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
-import { RodapeModule } from './componentes/rodape/rodape.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { CabecalhoModule } from "./componentes/cabecalho/cabecalho.module";
+import { RodapeModule } from "./componentes/rodape/rodape.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { RodapeModule } from './componentes/rodape/rodape.module';
     HttpClientModule,
     CabecalhoModule,
     RodapeModule,
+    AutenticacaoModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
