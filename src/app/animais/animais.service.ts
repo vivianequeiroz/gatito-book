4c3294ab14847e8cce02d3c4116d76fcfd6c99ef
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-18 14:35:59 -0300

    chore: observe obj to upload progress

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,16 +1,16 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, mapTo } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-import { TokenService } from '../autenticacao/token.service';
-import { Animais, Animal } from './animais';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, of, throwError } from "rxjs";
+import { catchError, mapTo } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+import { TokenService } from "../autenticacao/token.service";
+import { Animais, Animal } from "./animais";
 
 const API = environment.apiURL;
-const NOT_MODIFIED = '304';
+const NOT_MODIFIED = "304";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AnimaisService {
   constructor(private http: HttpClient, private tokenService: TokenService) {}
@@ -29,7 +29,7 @@ export class AnimaisService {
 
   curtir(id: number): Observable<boolean> {
     return this.http
-      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .post(`${API}/photos/${id}/like`, {}, { observe: "response" })
       .pipe(
         mapTo(true),
         catchError((error) => {
@@ -38,10 +38,15 @@ export class AnimaisService {
       );
   }
 
-  upload(descricao: string, permiteComentario: boolean, arquivo: File): void {
+  upload(descricao: string, permiteComentario: boolean, arquivo: File) {
     const formData = new FormData();
-    formData.append('description', descricao);
-    formData.append('allowComments', permiteComentario ? 'true' : 'false');
-    formData.append('imageFile', arquivo);
+    formData.append("description", descricao);
+    formData.append("allowComments", permiteComentario ? "true" : "false");
+    formData.append("imageFile", arquivo);
+
+    return this.http.post(`${API}/photos/upload`, formData, {
+      observe: "events",
+      reportProgress: true,
+    });
   }
 }
