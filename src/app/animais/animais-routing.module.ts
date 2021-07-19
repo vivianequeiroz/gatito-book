e0d8857071da5d8e8737b87d9a8c224af3dc6d42
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-19 10:42:51 -0300

    chore: preview template using ng template

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
-import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
-import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DetalheAnimalComponent } from "./detalhe-animal/detalhe-animal.component";
+import { ListaAnimaisComponent } from "./lista-animais/lista-animais.component";
+import { ListaAnimaisResolver } from "./lista-animais/lista-animais.resolver";
+import { NovoAnimalComponent } from "./novo-animal/novo-animal.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: ListaAnimaisComponent,
     resolve: {
       animais: ListaAnimaisResolver,
     },
   },
   {
-    path: ':animalId',
+    path: "novo",
+    component: NovoAnimalComponent,
+  },
+  {
+    path: ":animalId",
     component: DetalheAnimalComponent,
   },
 ];
---
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+
+@Component({
+  selector: "app-novo-animal",
+  templateUrl: "./novo-animal.component.html",
+  styleUrls: ["./novo-animal.component.css"],
+})
+export class NovoAnimalComponent implements OnInit {
+  formularioAnimal!: FormGroup;
+  file!: File;
+  preview!: string;
+  percentualConcluído = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  upload() {
+
+  }
+
+  gravaArquivo(arquivo: any) {
+    
+  }
+
+}
